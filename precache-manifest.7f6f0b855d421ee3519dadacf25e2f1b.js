f02325afee007c8a414a8022b5cbcf5bf0a3d479
hyeongjunk05 <hyeongjunk05@gmail.com>
2020-01-15 20:24:04 +0900

    Updates

---
+++ precache-manifest.7f6f0b855d421ee3519dadacf25e2f1b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1a3d9d09d609ab42ab7f1c6be51a3871",
+    "url": "/reactTodolist/index.html"
+  },
+  {
+    "revision": "a649256b837e011c2c5d",
+    "url": "/reactTodolist/static/css/main.a900f525.chunk.css"
+  },
+  {
+    "revision": "27bb40cb524655574a37",
+    "url": "/reactTodolist/static/js/2.a7c55534.chunk.js"
+  },
+  {
+    "revision": "8a14dcea7ad8b593c1e83c11f7ba1a89",
+    "url": "/reactTodolist/static/js/2.a7c55534.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a649256b837e011c2c5d",
+    "url": "/reactTodolist/static/js/main.d422abfc.chunk.js"
+  },
+  {
+    "revision": "3f199470b5c4e868f319",
+    "url": "/reactTodolist/static/js/runtime-main.6567e61e.js"
+  }
+]);
